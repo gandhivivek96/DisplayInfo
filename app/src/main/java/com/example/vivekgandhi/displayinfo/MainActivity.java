1ef16a6f843c0d7e04eb9fd453690de48d33f81c
Vivek Gandhi <vivek7772>
2017-03-12 12:02:38 +0530

    modified code for handling radio buttons  and added Alert Dialog .

--- app/src/main/java/com/example/vivekgandhi/displayinfo/MainActivity.java
+++ app/src/main/java/com/example/vivekgandhi/displayinfo/MainActivity.java
@@ -1,54 +1,52 @@
 package com.example.vivekgandhi.displayinfo;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
  EditText username;
     EditText email;
     TextView dispname;
     EditText pass,c,s;
-    String gender,uname,password,country,state,emailid;
+    String gender,uname,password,country,state,emailid,gen;
+    RadioButton maleRadioButton, femaleRadioButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void onRadioButtonClicked(View view)
+    public String onRadioButtonClicked(View view)
     {
+        maleRadioButton = (RadioButton) findViewById(R.id.male);
+        femaleRadioButton = (RadioButton) findViewById(R.id.female);
+        if(maleRadioButton.isChecked())
+        {
+            gender="male";
 
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.male:
-                if (checked) {
-
-                    gender="male";
-                    // Pirates are the best
-                    break;
-                }
-            case R.id.female:
-                if (checked) {
-
-                    gender="female";
-                    // Ninjas rule
-                    break;
-                }
         }
-
-
+        else if(femaleRadioButton.isChecked())
+        {
+            gender="female";
+        }
+        else
+        {
+            gender="null";
+        }
+        return gender;
     }
     public void display(View view)
     {
-        Intent i = new Intent(this,DisplayActivity.class);
+        Intent i = new Intent(this,SimpleLayout.class);
         username=(EditText) findViewById(R.id.uname);
         email=(EditText) findViewById(R.id.email);
         pass = (EditText) findViewById(R.id.password);
@@ -60,14 +58,26 @@ public class MainActivity extends AppCompatActivity {
         password = pass.getText().toString().trim();
         country = c.getText().toString().trim();
         state = s.getText().toString().trim();
-        i.putExtra("username",uname);
-        i.putExtra("password",password);
-        i.putExtra("email",emailid);
-        i.putExtra("country",country);
-        i.putExtra("state",state);
-        i.putExtra("gender",gender);
+        gen = onRadioButtonClicked(view);
 
-        startActivity(i);
 
+        if (emailid.isEmpty() || password.isEmpty() || uname.isEmpty() || country.isEmpty() || state.isEmpty() || gen.equals("null")) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage(R.string.error_message)
+                    .setTitle(R.string.error_title)
+                    .setPositiveButton(android.R.string.ok, null);
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+        else {
+             i.putExtra("username", uname);
+            i.putExtra("password", password);
+            i.putExtra("email", emailid);
+            i.putExtra("country", country);
+            i.putExtra("state", state);
+            i.putExtra("gender", gender);
+
+            startActivity(i);
+        }
     }
 }
--- app/src/main/java/com/example/vivekgandhi/displayinfo/SimpleLayout.java
+++ app/src/main/java/com/example/vivekgandhi/displayinfo/SimpleLayout.java
@@ -4,30 +4,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class DisplayActivity extends AppCompatActivity {
-TextView uname;
+public class SimpleLayout extends AppCompatActivity {
+
+
+    TextView uname;
     TextView email,pass,c,s;
     TextView gen;
     String emailid,name,password,country,state,gender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display);
+        setContentView(R.layout.activity_simple_layout);
+
 
         Bundle firstData = getIntent().getExtras();
-        uname = (TextView) findViewById(R.id.duname);
-        pass = (TextView) findViewById(R.id.dpassword);
-        email = (TextView) findViewById(R.id.demailid);
-        c = (TextView) findViewById(R.id.dcountry);
-        s = (TextView) findViewById(R.id.dstate);
-        gen = (TextView) findViewById(R.id.dgender);
+        uname = (TextView) findViewById(R.id.dduname);
+        pass = (TextView) findViewById(R.id.ddpassword);
+        email = (TextView) findViewById(R.id.ddemailid);
+        c = (TextView) findViewById(R.id.ddcountry);
+        s = (TextView) findViewById(R.id.ddstate);
+        gen = (TextView) findViewById(R.id.ddgender);
 
-         emailid = firstData.getString("email");
+        emailid = firstData.getString("email");
         name = firstData.getString("username");
         password = firstData.getString("password");
         country = firstData.getString("country");
         state = firstData.getString("state");
-         gender = firstData.getString("gender");
+        gender = firstData.getString("gender");
 
         uname.setText(name);
         pass.setText(password);
@@ -35,5 +38,6 @@ TextView uname;
         s.setText(state);
         gen.setText(gender);
         email.setText(emailid);
+
     }
 }
